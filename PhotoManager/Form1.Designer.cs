62ea34180df7e5814fbf60630f59548ba67fe9f8
Dan Haman <haman.dan@drukwerkdeal.nl>
2015-01-04 20:49:20 +0200

    + icon changed

--- PhotoManager/Form1.Designer.cs
+++ PhotoManager/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.backupSwitch = new System.Windows.Forms.CheckBox();
             this.label3 = new System.Windows.Forms.Label();
@@ -212,6 +213,7 @@
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "mainForm";
             this.Text = "photo-manager";
